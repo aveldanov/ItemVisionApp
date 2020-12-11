37fccea539d818e16a62be6b4485fc1ba42b08e8
Anton Veldanov <anton.veldanov@gmail.com>
2020-12-10 23:30:47 -0800

    iOS

--- ItemVisionApp/View/RoundedShadowView.swift
+++ ItemVisionApp/View/RoundedShadowView.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class RoundedShadowView: UIView {
 
-    override class func awakeFromNib() {
+    override func awakeFromNib() {
         self.layer.shadowColor = UIColor.darkGray.cgColor
         self.layer.shadowRadius = 15
+        self.layer.shadowOpacity = 0.75
+        self.layer.cornerRadius = self.frame.height/2
     }
 
 }
