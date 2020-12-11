a5b7a6eacf76f86d008d42ef22dd06ce0965834a
Anton Veldanov <anton.veldanov@gmail.com>
2020-12-10 20:53:05 -0800

    first commit

---
+++ ItemVisionApp/Controller/CameraViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ItemVisionApp
+//
+//  Created by Veldanov, Anton on 12/10/20.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+}
+
